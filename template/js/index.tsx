c12395a2b5d3ce2c3346a20c36adac10d5b463e9
hanxiaoxin <2431555290@qq.com>
2021-02-03 15:27:00 +0800

    version: 1.0.5

--- template/js/index.tsx
+++ template/js/index.tsx
@@ -1,15 +1,13 @@
-export const indexTemplate = `
-    import React from "react";
-    import ReactDOM from 'react-dom';
-    
-    function App() {
-        return (
-            <div>hello APUSIC!</div>
-        );
-    }
-    
-    ReactDOM.render(
-        App(),
-        document.getElementById('app')
+import React from "react";
+import ReactDOM from 'react-dom';
+
+function App() {
+    return (
+        <div>hello APUSIC!</div>
     );
-`;
+}
+
+ReactDOM.render(
+    App(),
+    document.getElementById('app')
+);
